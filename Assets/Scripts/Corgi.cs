31a114a516d0ba9112933388c93c99739a727b8d
Aidan Stewart <ahstewart06@icloud.com>
2026-03-03 15:46:06 -0800

    added gameparameters, corgi, keyboardinput

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 public class Corgi : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private SpriteRenderer spriteRenderer;
+
+    public void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 direction)
     {
-        
+        FaceCorrectDirection(direction);
+        Vector2 movementAmount = GameParameters.CorgiMoveSpeed * direction * Time.deltaTime;
+        spriteRenderer.transform.Translate(movementAmount.x, movementAmount.y, 0);
+    }
+
+    private void FaceCorrectDirection(Vector2 direction)
+    {
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
