5f176114dea7ab1e7ae6f7d9eb06c302c7c1e6e6
Aidan Stewart <ahstewart06@icloud.com>
2026-04-02 15:40:04 -0700

    Added GameTimer and GameOver screen

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,21 +1,41 @@
+using System;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
     public UI Ui;
+    public GameTimer GameTimer;
     
     void Start()
     {
-        
+        Ui.HideGameOverScreen();
+        Ui.ShowStartScreen();
     }
     
     void Update()
     {
-        
+        Ui.ShowTime();
     }
 
     public void OnStartButtonClicked()
     {
         Ui.HideStartScreen();
+        StartGame();
+    }
+
+    public void OnPlayAgainButtonClicked()
+    {
+        Ui.HideGameOverScreen();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        GameTimer.StartTimer(10, OnTimerFinished);
+    }
+
+    public void OnTimerFinished()
+    {
+        Ui.ShowGameOverScreen();
     }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,14 +5,37 @@ public class UI : MonoBehaviour
 {
     public TMP_Text ScoreText;
     public CanvasGroup StartScreenCanvasGroup;
+    public CanvasGroup GameOverScreenCanvasGroup;
+    public TMP_Text TimeText;
+    public GameTimer GameTimer;
     
     public void SetScoreText(int score)
     {
         ScoreText.text = "Score: " + score;
     }
 
+    public void ShowTime()
+    {
+        TimeText.text = GameTimer.GetTimeAsString();
+    }
+
     public void HideStartScreen()
     {
         CanvasGroupDisplayer.Hide(StartScreenCanvasGroup);
+    }
+
+    public void ShowStartScreen()
+    {
+        CanvasGroupDisplayer.Show(StartScreenCanvasGroup);
+    }
+
+    public void HideGameOverScreen()
+    {
+        CanvasGroupDisplayer.Hide(GameOverScreenCanvasGroup);
+    }
+
+    public void ShowGameOverScreen()
+    {
+        CanvasGroupDisplayer.Show(GameOverScreenCanvasGroup);
     }
 }
